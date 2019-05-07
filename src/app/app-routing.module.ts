4f8825415bcf490c3442307e369a14a5a74f6c11
Giuseppe Matteoni <giuseppe.matteoni@avanade.com>
2019-05-07 12:43:35 -0300

    ajustes assets + cadastro inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FormLoginComponent } from './main-content/form-login/form-login.component';
+import { FormCadastroComponent } from './main-content/form-cadastro/form-cadastro.component'; 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent, pathMatch: 'full'},
   { path: 'content', component: MainContentComponent, children: [
     {
-      path: '', component: FormLoginComponent
+      path: 'login', component: FormLoginComponent
+    },
+    {
+      path: 'cadastro', component: FormCadastroComponent
     }
   ]}
   
